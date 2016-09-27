b7177606a7ee45d21129fb32d33e05d86f9834e0
Dor Rubin <dor.rubin@gmail.com>
2016-09-27 14:11:45 -0400

    Initial Commit

---
+++ oauth-evernote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oauth-evernote
+//
+//  Created by Dor Rubin on 9/27/16.
+//  Copyright (c) 2016 Dor Rubin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ oauth-evernoteTests/oauth_evernoteTests.swift
@@ -0,0 +1,36 @@
+//
+//  oauth_evernoteTests.swift
+//  oauth-evernoteTests
+//
+//  Created by Dor Rubin on 9/27/16.
+//  Copyright (c) 2016 Dor Rubin. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class oauth_evernoteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
