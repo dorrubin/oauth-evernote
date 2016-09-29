862418c07b6d5e77d6302be726e8aab9926dfbef
Dor Rubin <dor.rubin@gmail.com>
2016-09-29 13:25:00 -0400

    connected ui to code

--- oauth-evernote/ViewController.swift
+++ oauth-evernote/ViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle text field callback
+        nameTextField.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide keyboard
+        textField.resignFirstResponder()
+        return true
     }
-
-
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 }
 
