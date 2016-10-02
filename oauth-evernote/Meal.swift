918c463c753a19ec2365b9f595c617ddfb51714e
Dor Rubin <dor.rubin@gmail.com>
2016-10-01 21:07:40 -0400

    unit tests

---
+++ oauth-evernote/Meal.swift
@@ -0,0 +1,29 @@
+//
+//  Meal.swift
+//  oauth-evernote
+//
+//  Created by Dor Rubin on 10/1/16.
+//  Copyright © 2016 Dor Rubin. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    var name: String
+    var photo: UIImage
+    var rating: Int
+    
+    // MARK: Initialization
+    init?(name: String, photo: UIImage? = nil, rating: Int) {
+        self.name = name
+        self.rating = rating
+        self.photo = (photo)!
+        
+        
+        // Failed initialization
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
