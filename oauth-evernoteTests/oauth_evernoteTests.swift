6189826cfa20b26f6333aa242e2bf21448911cab
Dor Rubin <dor.rubin@gmail.com>
2016-10-02 23:45:38 -0400

    fixed unit test

--- oauth-evernoteTests/oauth_evernoteTests.swift
+++ oauth-evernoteTests/oauth_evernoteTests.swift
@@ -12,16 +12,16 @@ import XCTest
 class oauth_evernoteTests: XCTestCase {
 
     func testMealInitialization(){
-//        //pass test
-//        let potentialItem = Meal(name: "New meal", photo: nil, rating: 5)
-//        XCTAssertNotNil(potentialItem)
-//
-//        //no name test
-//        let noName = Meal(name: "", photo: nil, rating: 0)
-//        XCTAssertNil(noName, "Empty name is invalid")
-//        
-//        //bad rating
-//        let badRating = Meal(name: "Bad bad", photo: nil, rating: -4)
-//        XCTAssertNotNil(badRating)
+        //pass test
+        let potentialItem = Meal(name: "New meal", photo: nil, rating: 5)
+        XCTAssertNotNil(potentialItem)
+
+        //no name test
+        let noName = Meal(name: "", photo: nil, rating: 0)
+        XCTAssertNil(noName, "Empty name is invalid")
+        
+        //bad rating
+        let badRating = Meal(name: "Bad bad", photo: nil, rating: -4)
+        XCTAssertNotNil(badRating, "Should not be negative")
     }
 }
