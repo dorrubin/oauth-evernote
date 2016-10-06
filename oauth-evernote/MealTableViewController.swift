447a98f27c8868ac261be340b8cab2a45354b7a6
Dor Rubin <dor.rubin@gmail.com>
2016-10-06 12:31:10 -0400

    delete row

--- oauth-evernote/MealTableViewController.swift
+++ oauth-evernote/MealTableViewController.swift
@@ -15,7 +15,9 @@ class MealTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        navigationItem.leftBarButtonItem = editButtonItem()
+        
         //load sample data
         loadSampleMeals()
     }
@@ -70,17 +72,18 @@ class MealTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            meals.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
